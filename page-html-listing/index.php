34f024bf7ed74607fe6735fd260d04379bb82b5a
Jessy <jessy.jeannin@gmail.com>
2018-12-12 16:13:18 +0100

    page html listing

---
+++ page-html-listing/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Cairo" rel="stylesheet">
+    <title>Page de listing</title>
+</head>
+<body>
+
+<div><img src="./images-listing/header-image.jpg" alt=""></div>
+
+
+<div class="date-listing"><p>Année <span class="years-listing">(mettre la date)</span></p></div>
+
+    <div class="div-listing">
+        <ul class="ul-listing">
+            <li class="li-listing"></li>
+        </ul>
+    </div>
+
+<footer><p>(c)Copyright Fait par Thomas, Vincent, Antoine, Marc, Théo, Aymeric, Rémi, Jonathan Directionné par Thomas Vinchent et fait à AIFOR à ETAPLES SUR MER dans le 62630 vers ~15h30. Il fait relativement beau, il y a du soleil et il fait très froid mais il ne pleut pas. La température dehors est d'envoirons 3, 4°.</p></footer>
+</body>
+</html>

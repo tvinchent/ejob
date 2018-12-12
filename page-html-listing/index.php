51d9de97794d258180514b4b9d2a154ff996bd50
Jessy <jessy.jeannin@gmail.com>
2018-12-12 16:45:08 +0100

    listing page part 2

---
+++ page-html-listing/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./commun.css">
+    <link href="https://fonts.googleapis.com/css?family=Cairo" rel="stylesheet">
+    <title>Page de listing</title>
+</head>
+<body>
+
+<div class="date-listing"><p>Année <span class="years-listing"><a href="#">2018</a></span></p></div>
+
+    <div class="div-listing">
+        <ul class="ul-listing">
+            <img class="trombinoscope" src="" alt=""><li class="li-listing"></li>
+            <img class="trombinoscope" src="" alt=""><li class="li-listing"></li>
+            <img class="trombinoscope" src="" alt=""><li class="li-listing"></li>
+            <img class="trombinoscope" src="" alt=""><li class="li-listing"></li>
+            <img class="trombinoscope" src="" alt=""><li class="li-listing"></li>
+            <img class="trombinoscope" src="" alt=""><li class="li-listing"></li>
+            <img class="trombinoscope" src="" alt=""><li class="li-listing"></li>
+            <img class="trombinoscope" src="" alt=""><li class="li-listing"></li>
+            <img class="trombinoscope" src="" alt=""><li class="li-listing"></li>
+            <img class="trombinoscope" src="" alt=""><li class="li-listing"></li>
+            <img class="trombinoscope" src="" alt=""><li class="li-listing"></li>
+        </ul>
+    </div>
+
+<footer><p>(c)Copyright Fait par Thomas, Vincent, Antoine, Marc, Théo, Aymeric, Rémi, Jonathan Directionné par Thomas Vinchent</p></footer>
+</body>
+</html>

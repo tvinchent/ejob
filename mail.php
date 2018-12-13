67590f33d76d3ffd980cce33347047fa40c92690
Vincent <the-dede@hotmail.fr>
2018-12-13 16:20:29 +0100

    mail corrigé

--- mail.php
+++ mail.php
@@ -1,16 +1,21 @@
 <?php
-	if (isset($_POST['envoie'])) {
-		if (!isset($_POST['subject']) || $_POST['subject']=='') {
-			echo('Vous avez oublié d\'ins&eacute;rer un message<br>');
-		}
-		else{
-			// assignation de la varaiable mail si aucune adresse mail renseignée
-			if (!isset($_POST['email']) || $_POST['email']=='') {
-				$_POST['email']='';
-			}
 
-			$message = 'Vous avez recu un message via votre site internet, le voici:<br>'.$_POST['subject'];
+	if (!empty($_POST)) {
+        $errors = array();
 
+		if (empty($_POST['subject'])) {
+			$errors['subject'] = "Vous avez oublié d\'ins&eacute;rer un message<br>";
+        }
+        if (empty($_POST['email'])) {
+			$errors['email'] = "Vous avez oublié d\'ins&eacute;rer un email<br>";
+        }
+        if (empty($_POST['request'])) {
+			$errors['request'] = "Vous avez oublié d\'ins&eacute;rer un sujet<br>";
+        }
+        
+        if(isset($_POST['subject'], $_POST['email'], $_POST['request']) AND empty($errors)) {
+
+            $message = 'Vous avez recu un message via votre site internet, le voici:<br>'.$_POST['subject'];
 			$headers  = 'MIME-Version: 1.0' . "\r\n";
 			$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
 			$headers .= 'From: '.$_POST['email']."\r\n".'Reply-To: '.$_POST['email']."\r\n".'X-Mailer: PHP/' . phpversion();
@@ -19,8 +24,18 @@
 
 			// confirmation
 			echo('Votre message a &eacute;t&eacute; envoy&eacute;<br>');
-		}
-	}
-
-
+        
+        }
+    }
 ?>
+
+<?php if(!empty($errors)): ?>
+
+<div>
+    <ul>
+        <?php foreach($errors as $error): ?>
+        <li><?= $error; ?></li>
+<?php endforeach; ?>
+    </ul>
+</div>
+<?php endif; ?>

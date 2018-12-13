c0de2a5b11f846951e9bd1110fc519620c61fda4
Vincent <the-dede@hotmail.fr>
2018-12-13 08:50:35 +0100

    Auto stash before merge of "master" and "origin/master"

--- index.php
+++ index.php
@@ -20,7 +20,11 @@
     </div>
 
 <footer>
+<<<<<<< Updated upstream
     <p>(c)Copyright Fait par Thomas, Vincent, Antoine, Marc, Théo, Aymeric, Rémi, Jonathan Directionné par Thomas Vinchent et fait à AIFOR à ETAPLES SUR MER dans le 62630.</p>
+=======
+    <p>(c)Copyright Fait par Thomas, Vincent, Antoine, Marc, Théo, Aymeric, Rémi, Jonathan Directionné par Thibault Vinchent et fait à AIFOR à ETAPLES SUR MER dans le 62630 vers ~15h30. Il fait relativement beau, il y a du soleil et il fait très froid mais il ne pleut pas. La température dehors est d'environs 3, 4°.</p>
+>>>>>>> Stashed changes
 </footer>
 </body>
 </html>

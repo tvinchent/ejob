5ca6d28c0329253053a6238057a9ff4c87698ce2
Vincent <the-dede@hotmail.fr>
2018-12-13 09:45:41 +0100

    index link

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Cairo" rel="stylesheet">
     <link rel="stylesheet" href="css/listing.css">
+    <link rel="stylesheet" href="css/commun.css">
     <title>Page de listing</title>
 </head>
 <body>

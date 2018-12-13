b8e447f3d601642fedc16eb594280f6b353e086e
Tyrael de la sainte bagarre <thomas.antoine59@outlook.fr>
2018-12-13 09:22:27 +0100

    css comm

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./commun.css">
     <link href="https://fonts.googleapis.com/css?family=Cairo" rel="stylesheet">
+    <link rel="stylesheet" href="./css/commun.css">
     <title>Page de listing</title>
 </head>
 <body>
-
+<div class="contenue">
 <div class="date-listing">
 
 <p>Année <span class="years-listing"><a href="#">2017</a></span></p>
@@ -32,7 +32,7 @@
             <img class="trombinoscope" src="" alt=""><li class="li-listing"></li>
         </ul>
     </div>
-
+    </div>
 <footer><p>(c)Copyright Fait par Thomas, Vincent, Antoine, Marc, Théo, Aymeric, Rémi, Jonathan Directionné par Thomas Vinchent</p></footer>
 </body>
 </html>

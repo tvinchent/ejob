d5c9c5bc34a3bc9574c6c4c68607cd8497b3371c
Vincent <the-dede@hotmail.fr>
2018-12-13 09:53:45 +0100

    repull

--- index.php
+++ index.php
@@ -20,26 +20,6 @@
 
     <div class="div-listing">
         <ul class="ul-listing">
-<<<<<<< HEAD
-            <img class="trombinoscope" src="images/cv.png" alt=""><li class="li-listing">Tomas Passeleu</li><br/>
-            <img class="trombinoscope" src="images/cv.png" alt=""><li class="li-listing">Vincent Descharles</li><br/>
-            <img class="trombinoscope" src="images/cv.png" alt=""><li class="li-listing">Antoine Thomas</li><br/>
-            <img class="trombinoscope" src="images/cv.png" alt=""><li class="li-listing">Marc Boucher</li><br/>
-            <img class="trombinoscope" src="images/cv.png" alt=""><li class="li-listing">Théo Guérinot</li><br/>
-            <img class="trombinoscope" src="images/cv.png" alt=""><li class="li-listing">Aymeric Delcampe</li><br/>
-            <img class="trombinoscope" src="images/cv.png" alt=""><li class="li-listing">Rémi Mouillard</li><br/>
-            <img class="trombinoscope" src="images/cv.png" alt=""><li class="li-listing">Jonathan Macret</li><br/>
-            <img class="trombinoscope" src="images/cv.png" alt=""><li class="li-listing">Jesse Jeannin</li><br/>
-            <img class="trombinoscope" src="images/cv.png" alt=""><li class="li-listing">Maxime Arbouille</li><br/>
-            <img class="trombinoscope" src="images/cv.png" alt=""><li class="li-listing">Bryan Reisenleiter</li><br/>
-        </ul>
-    </div>
-    </div>
-   <br/>
-   <br/>
-   <br/>
-    <footer><p>(c)Copyright Fait par Thomas, Vincent, Antoine, Marc, Théo, Aymeric, Rémi, Jonathan Directionné par Thomas Vinchent</p></footer>
-=======
             <li class="li-listing"><a href="https://www.creeruncv.com/sites/default/files/124-cv-pole-emploi.png">Thomas Passeleu</a></li>
             <li class="li-listing"><a href="https://www.creeruncv.com/sites/default/files/124-cv-pole-emploi.png">Vincent Descharles</a></li>
             <li class="li-listing"><a href="https://www.creeruncv.com/sites/default/files/124-cv-pole-emploi.png">Antoine Thomas</a></li>
@@ -57,6 +37,5 @@
 <footer>
     <p>(c)Copyright Fait par Thomas, Vincent, Antoine, Marc, Théo, Aymeric, Rémi, Jonathan. Dirigé par Thibault Vinchent</p>
 </footer>
->>>>>>> master
 </body>
 </html>
